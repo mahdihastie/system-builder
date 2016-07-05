6c44ca87c5a46cded922aadeac344594af4ba8cd
Mahdi Hastie <mahdi.hastie@gmail.com>
2016-07-05 15:20:40 +0100

    installers

---
+++ cask-iq.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install native apps
+brew tap caskroom/versions
+
+brew cask install atom
+brew cask install batchmod
+brew cask install caffeine
+brew cask install cheatsheet
+brew cask install chrome-remote-desktop-host
+brew cask install coconutbattery
+brew cask install cyberduck
+brew cask install daisydisk
+brew cask install easyfind
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install hosts
+brew cask install lastpass
+brew cask install ip-in-menu-bar
+brew cask install iterm2
+brew cask install skype
+brew cask install slack
+brew cask install screenhero
+brew cask install textwrangler
+brew cask install teamviewer
+brew cask install vlc
