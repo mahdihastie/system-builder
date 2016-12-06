2fdec4cbabe0428ceb25589a96d0c377280cea09
mahdi hastie <mahdihastie@users.noreply.github.com>
2016-12-06 20:31:51 +0000

    new master list

---
+++ cask.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Install native apps
+brew tap caskroom/versions
+
+brew cask install 1password
+brew cask install android-file-transfer
+brew cask install atom
+brew cask install bartender
+brew cask install batchmod
+brew cask install calibre
+brew cask install coconutbattery
+brew cask install cyberduck
+brew cask install daisydisk
+brew cask install dropbox
+brew cask install dukto
+brew cask install evernote
+brew cask install easyfind
+brew cask install firefox
+brew cask install go2shell
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install hosts
+brew cask install ifunbox
+brew cask install ip-in-menu-bar
+brew cask install iterm2
+brew cask install istat-menus
+brew cask install numi
+brew cask install mactracker
+brew cask install middleclick
+brew cask install nordvpn
+brew cask install oversight
+brew cask install radio-silence
+brew cask install sdformatter
+brew cask install slack
+brew cask install screenhero
+brew cask install spotify
+brew cask install soundflower
+brew cask install textwrangler
+brew cask install vlc
+brew cask install utorrent
+brew cask install teamviewer
