d1a72a2354ea262f217e8432f952597db2c56f15
Mahdi Hastie <mahdi.hastie@gmail.com>
2016-12-20 11:53:12 +0000

    media server cask

---
+++ cask-server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install native apps
+brew tap caskroom/versions
+
+brew cask install atom
+brew cask install batchmod
+brew cask install dukto
+brew cask install easyfind
+brew cask install hosts
+brew cask install kodi
+brew cask install teamviewer
