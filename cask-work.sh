18d80a9eb1df93801d412f21993d73e1a1fd6bcf
mahdi hastie <mahdihastie@users.noreply.github.com>
2017-10-11 13:57:34 +0100

    updates

--- cask-work.sh
+++ cask-work.sh
@@ -5,7 +5,7 @@ brew tap caskroom/versions
 
 brew cask install atom
 brew cask install batchmod
-brew cask install caffeine
+brew cask install keepingyouawake
 brew cask install cheatsheet
 brew cask install chrome-remote-desktop-host
 brew cask install coconutbattery
@@ -17,7 +17,6 @@ brew cask install fliqlo
 brew cask install firefox
 brew cask install google-chrome
 brew cask install hosts
-brew cask install lastpass
 brew cask install ip-in-menu-bar
 brew cask install iterm2
 brew cask install slack
@@ -25,3 +24,7 @@ brew cask install screenhero
 brew cask install avast-mac-security
 brew cask install vlc
 brew cask install teamviewer-quicksupport
+brew cask install 1password
+brew cask install adapter
+brew cask install onedrive
+brew cask install graphicconverter
