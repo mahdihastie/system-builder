a5e8d5355d3c35affd8b9f8edc2fbd444a14798a
mahdi hastie <mahdihastie@users.noreply.github.com>
2018-09-09 16:18:40 +0100

    updates
    remove hosts & creating generic

--- cask-misc.sh
+++ cask-misc.sh
@@ -4,23 +4,13 @@
 brew tap caskroom/versions
 
 # brew cask install atom
-brew cask install batchmod
 brew cask install keepingyouawake
-brew cask install cheatsheet
 brew cask install coconutbattery
-brew cask install cyberduck
-brew cask install daisydisk
 brew cask install dukto
 brew cask install easyfind
 brew cask install fliqlo
 brew cask install firefox
 brew cask install google-chrome
-brew cask install ip-in-menu-bar
-brew cask install iterm2
-brew cask install slack
 brew cask install vlc
-brew cask install teamviewer-quicksupport
 brew cask install adapter
-brew cask install onedrive
 brew cask install microsoft-office
-brew cask install microsoft-remote-desktop-beta
