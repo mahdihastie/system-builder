ac54313baada0c708644dcfa641f04fd43c073c1
mahdi hastie <mahdihastie@users.noreply.github.com>
2021-01-04 22:07:36 +0000

    Create cask-jkr.sh
    jkr build

---
+++ cask-jkr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# Install native apps
+brew tap caskroom/versions
+
+brew cask install cyberduck
+brew cask install firefox
+brew cask install google-chrome
+brew cask install slack
+brew cask install spotify
+brew cask install vlc
+brew cask install microsoft-teams
+brew cask install visual-studio-code
+brew cask install
+brew cask microsoft-office
+brew cask zoom
