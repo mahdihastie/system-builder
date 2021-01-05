7239a17dc9f3ec98d6f9a9e98523d3a458547c58
mahdi hastie <mahdihastie@users.noreply.github.com>
2021-01-05 13:45:51 +0000

    alphabetical

--- cask-jkr.sh
+++ cask-jkr.sh
@@ -6,10 +6,10 @@ brew tap caskroom/versions
 brew install --cask cyberduck
 brew install --cask firefox
 brew install --cask google-chrome
+brew install --cask microsoft-teams
+brew install --cask microsoft-office
 brew install --cask slack
 brew install --cask spotify
 brew install --cask vlc
-brew install --cask microsoft-teams
 brew install --cask visual-studio-code
-brew install --cask microsoft-office
 brew install --cask zoom
